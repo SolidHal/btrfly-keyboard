494b0da4735a8a7d5028c4cc05c5125672e77419
SolidHal <hal@halemmerich.com>
2022-11-12 14:12:20 -0600

    bring keymap in line with btrfld
    - move volume keys
    - add bootloader key
    - add more FN keys

--- qmk/btrfly/keymaps/default/keymap.c
+++ qmk/btrfly/keymaps/default/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q, KC_W, KC_E, KC_R, KC_T,                 KC_Y, KC_U, KC_I, KC_O, KC_P, \
         KC_A, KC_S, KC_D, KC_F, KC_G,                 KC_H, KC_J, KC_K, KC_L, KC_SCLN, \
         KC_Z, KC_X, KC_C, KC_V, KC_B,                 KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, \
-              KC_NO, KC_NO,                                       KC_LALT, KC_NO, \
+              KC_VOLD, KC_VOLU,                                   KC_LALT, KC_DEL, \
                                  KC_LGUI,             KC_ENT,\
                            MO(1), KC_SPC,             KC_LSFT, MO(2),\
                          KC_LCTL, KC_ESC,             KC_BSPC,KC_TAB \
@@ -45,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       ),
 
         [2] = LAYOUT( \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, \
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, DF(3), \
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,            KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, \
+      QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO,            KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_F11, \
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            KC_NO,   KC_NO,   KC_NO, KC_NO, DF(3), \
                KC_TRNS, KC_TRNS,                                        KC_TRNS, KC_TRNS, \
                                       KC_TRNS,        KC_TRNS,
                              KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS,
@@ -66,9 +66,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                      ),
 
         [4] = LAYOUT( \
-                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, \
-                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, \
-                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, DF(0), \
+                     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,            KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, \
+                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_F11, \
+                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,            KC_NO,   KC_NO,   KC_NO, KC_NO, DF(0), \
                             KC_TRNS, KC_TRNS,                                        KC_TRNS, KC_TRNS, \
                                           KC_TRNS,                 KC_TRNS,
                                           KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS,
